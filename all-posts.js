0d8905be5ad77c57ba99bd9d5e76fbd13c3b3d7c
Siw Elin Årolilja Iversen <111748314+Psijii@users.noreply.github.com>
2023-04-25 15:02:59 +0200

    whateven

--- all-posts.js
+++ all-posts.js
@@ -1,14 +1,18 @@
+const modal = document.getElementById("image-modal");
+const modalImg = document.getElementById("modal-image");
+
+function openModal(imageUrl) {
+  modal.style.display = "block";
+  modalImg.src = imageUrl;
+}
+
 async function fetchAllPosts() {
   const response = await fetch("https://examone.techlilja.io/wp-json/wp/v2/posts?_embed&per_page=100");
   const data = await response.json();
-  return data;
-}
 
-async function displayAllPosts() {
-  const posts = await fetchAllPosts();
   const allPostsContainer = document.getElementById("all-posts-container");
 
-  posts.forEach((post) => {
+  data.forEach((post) => {
     const postWrapper = document.createElement("div");
     postWrapper.classList.add("post");
 
@@ -20,18 +24,20 @@ async function displayAllPosts() {
 
     postWrapper.appendChild(title);
     postWrapper.appendChild(content);
+
+    // Check if post has an image
+    if (post._embedded && post._embedded["wp:featuredmedia"]) {
+      const imageUrl = post._embedded["wp:featuredmedia"][0].source_url;
+      const image = document.createElement("img");
+      image.src = imageUrl;
+      image.classList.add("post-image");
+      postWrapper.appendChild(image);
+    }
+
     allPostsContainer.appendChild(postWrapper);
   });
 
   handleModalBehavior();
-}
-
-const modal = document.getElementById("image-modal");
-const modalImg = document.getElementById("modal-image");
-
-function openModal(imageUrl) {
-  modal.style.display = "block";
-  modalImg.src = imageUrl;
 }
 
 function handleModalBehavior() {
@@ -48,4 +54,4 @@ function handleModalBehavior() {
   });
 }
 
-displayAllPosts();
+fetchAllPosts();
