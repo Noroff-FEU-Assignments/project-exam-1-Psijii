29ee2ec478bb2e8aefe9f760fff880a3d606da66
Siw Elin Årolilja Iversen <111748314+Psijii@users.noreply.github.com>
2023-04-25 12:39:46 +0200

    comment test

--- fetch-post.js
+++ fetch-post.js
@@ -20,3 +20,42 @@ async function displayPost() {
 }
 
 displayPost();
+
+document.getElementById("comment-form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const postId = sessionStorage.getItem("selectedPostId");
+  const author = document.getElementById("comment-author").value;
+  const email = document.getElementById("comment-email").value;
+  const content = document.getElementById("comment-content").value;
+
+  try {
+    await submitComment(postId, author, email, content);
+    alert("Comment submitted successfully!");
+    document.getElementById("comment-form").reset();
+  } catch (error) {
+    console.error("Failed to submit comment", error);
+    alert("Failed to submit comment. Please try again.");
+  }
+});
+
+async function submitComment(postId, author, email, content) {
+  const response = await fetch("https://examone.techlilja.io/wp-json/wp/v2/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      post: postId,
+      author_name: author,
+      author_email: email,
+      content: content,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to submit comment");
+  }
+
+  return await response.json();
+}
